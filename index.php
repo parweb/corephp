fb718e78271d8af3603559b4eef5e5ca0d1aa329
Gabriel Sobrinho <gabriel.sobrinho@gmail.com>
2009-08-03 23:17:18 -0300

    First commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Core PHP Framework
+ * Copyright (C) 2008-2009 Gabriel Sobrinho <gabriel@corephp.org>
+ *
+ * This file is part of Core PHP Framework.
+ *
+ * Core PHP Framework is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Core PHP Framework is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Core PHP Framework. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    Core
+ * @copyright  2008-2009 Gabriel Sobrinho <gabriel@corephp.org>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html GNU Lesser General Public License version 3 (GPLv3)
+ * @version    0.1
+ */
+
+// Buffer and error reporting
+ob_start ();
+error_reporting ( E_ALL | E_STRICT );
+
+// Set include path
+$include_path = implode ( PATH_SEPARATOR, array_map ( 'realpath', array ( 'lib' ) ) );
+set_include_path ( $include_path );
+
+unset ( $include_path );
+
+// Autoload
+require 'functions.php';
+
+Config::parseApplicationFiles ();
+
